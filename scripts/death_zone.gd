d0e2349634983a15133dce1d9dd19f51d6c6455c
Carvalhop2 <wellingtonp2.oc@gmail.com>
2025-06-05 20:27:29 -0300

    quase pronto

--- scripts/death_zone.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-func _on_body_entered(body: CharacterBody2D) -> void:
-	if body.is_in_group("Player"):
-		
-		print("⚠️ O jogador morreu!")
-		
-func _on_death_() -> void:
-	get_tree().reload_current_scene()
