44b7c9f3e2f031dfe7a6e7ddb4a39c78a8231ff3
ZcvGuilherme <guisousasilvanota10@gmail.com>
2025-06-15 01:14:37 -0300

    funcionando e emitindo sinal

--- scripts/wall_blades.gd
+++ scripts/wall_blades.gd
@@ -1,18 +1,28 @@
 extends Area2D
 
+@export var active_duration: float = 1.0
+@export var repeat_interval: float = 2.0
+
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-
-
+@onready var timer: Timer = $Timer
 
 func _ready() -> void:
-	# Conecta o sinal pra saber quando uma animação acabou
-	animation_player.animation_finished.connect(_on_animation_finished)
-	# Começa pelo activate
+	animation_player.animation_finished.connect(_on_animation_player_animation_finished)
+	timer.wait_time = repeat_interval
+	timer.timeout.connect(_on_timer_timeout)
+	timer.start()
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "activating":
+		animation_player.play("active")
+		get_tree().create_timer(active_duration).timeout.connect(deactive)
+	elif anim_name == "deactivate":
+		timer.start()
+func deactive():
+	animation_player.play("deactivate")
+func _on_timer_timeout() -> void:
 	animation_player.play("activating")
 
-func _on_animation_finished(anim_name: String) -> void:
-	if anim_name == "activating":
-		# Quando o activate finalizar, tocamos o active
-		animation_player.play("active")
-	elif anim_name == "active":
-		animation_player.play("deactivate")
+func _on_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		body.die()
