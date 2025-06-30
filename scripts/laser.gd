28d7a4ec86fc979ba01ff8263ad973a4412128c8
ZcvGuilherme <guisousasilvanota10@gmail.com>
2025-06-30 13:28:40 -0300

    lase/turret adicionados

--- scripts/laser.gd
+++ scripts/laser.gd
@@ -1,10 +1,10 @@
-extends Area2D
+extends Node2D
+
 @export var active_duration: float = 1.0
 @export var repeat_interval: float = 2.0
 @export var eternal := false
 
-@onready var sprite_2d: Sprite2D = $Sprite2D 
-@onready var collision: CollisionShape2D = $Collision 
+@onready var area_2d: Area2D = $Area2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var timer: Timer = $Timer
 
@@ -15,22 +15,20 @@ func _ready() -> void:
 		timer.stop()
 	else:
 		timer.start()
-	
-func _on_body_entered(body: Node2D) -> void:
+		
+func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		body.die()
-
-
+		
 func _on_timer_timeout() -> void:
-	animation_player.play("activate")
+	animation_player.play("activating")
 
-func deactive():
-	animation_player.play("deactivate")
-
+func deactivate():
+	animation_player.play("deactivating")
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "activate":
 		animation_player.play("idle")
-		get_tree().create_timer(active_duration).timeout.connect(deactive)
+		get_tree().create_timer(active_duration).timeout.connect(deactivate)
 	elif anim_name == "deactivate":
 		if repeat_interval > 0:
 			timer.start()
