1362eee6a49d60acc6254a0d3afb17ee1b06fd24
ZcvGuilherme <guisousasilvanota10@gmail.com>
2025-07-05 22:58:54 -0300

    quase lá

--- scripts/laser.gd
+++ scripts/laser.gd
@@ -1,20 +1,20 @@
 extends Node2D
 
-@export var active_duration: float = 1.0
+@export var active_duration: float = 2.0
 @export var repeat_interval: float = 2.0
 @export var eternal := false
 
 @onready var area_2d: Area2D = $Area2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var timer: Timer = $Timer
+@onready var repeat_timer: Timer = $repeat_timer
 
 func _ready() -> void:
-	timer.wait_time = repeat_interval
+	repeat_timer.wait_time = repeat_interval
 	if eternal:
 		animation_player.play("idle")
-		timer.stop()
+		repeat_timer.stop()
 	else:
-		timer.start()
+		animation_player.play("activating")
 		
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
@@ -26,9 +26,10 @@ func _on_timer_timeout() -> void:
 func deactivate():
 	animation_player.play("deactivating")
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	if anim_name == "activate":
+	if anim_name == "activating":
 		animation_player.play("idle")
 		get_tree().create_timer(active_duration).timeout.connect(deactivate)
-	elif anim_name == "deactivate":
+	elif anim_name == "deactivating":
 		if repeat_interval > 0:
-			timer.start()
+			animation_player.play("unactive")
+			repeat_timer.start()
